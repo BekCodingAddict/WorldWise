f05c6dd52e2c9c31a1561e2194625c07b6c776ee
BekCodingAddict <experte001@gmail.com>
2024-09-19 00:50:19 +0900

    Linking between Routes with Link and NavLink has been done

--- client/src/pages/PageNotFound.jsx
+++ client/src/pages/PageNotFound.jsx
@@ -1,5 +1,12 @@
+import PageNav from "../components/PageNav";
+
 function PageNotFound() {
-  return <div>Page not found 😰</div>;
+  return (
+    <div>
+      <PageNav />
+      Page not found 😰
+    </div>
+  );
 }
 
 export default PageNotFound;
