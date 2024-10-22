5cff1e4b0081531825437695e157cd05ec4b0334
unknown <experte001@gmail.com>
2024-10-22 18:53:31 +0900

    Mobile Hamburger Menu has been designed!

--- client/src/components/PageNav.jsx
+++ client/src/components/PageNav.jsx
@@ -2,12 +2,23 @@ import { NavLink } from "react-router-dom";
 import styles from "./PageNav.module.css";
 import Logo from "./Logo";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { useState } from "react";
+import { FaArrowRight } from "react-icons/fa6";
 
 function PageNav() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <nav className={styles.nav}>
       <Logo />
-      <GiHamburgerMenu className={styles.menu} />
+
+      {isOpen ? (
+        <PageNavMobile setIsOpen={setIsOpen} isOpen={isOpen} />
+      ) : (
+        <GiHamburgerMenu
+          className={styles.menu}
+          onClick={() => setIsOpen(!isOpen)}
+        />
+      )}
       <ul>
         <li>
           <NavLink to="/pricing">Pricing</NavLink>
@@ -22,6 +33,30 @@ function PageNav() {
         </li>
       </ul>
     </nav>
+  );
+}
+
+function PageNavMobile({ setIsOpen, isOpen }) {
+  return (
+    <div className={`${styles.mobileNavContainer} ${isOpen ? "open" : ""}`}>
+      <FaArrowRight
+        className={styles.mobileBackBtn}
+        onClick={() => setIsOpen((show) => !show)}
+      />
+      <nav className={styles.mobileNav}>
+        <ul>
+          <li>
+            <NavLink to="/pricing">Pricing</NavLink>
+          </li>
+          <li>
+            <NavLink to="/Product">Product</NavLink>
+          </li>
+          <li>
+            <NavLink to="/login">Login</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 }
 
