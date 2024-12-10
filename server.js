b0c3615be9631606026c685553a00a8cf9dcae24
unknown <experte001@gmail.com>
2024-12-11 03:19:25 +0900

    MongoDb connection setUp has been done

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-require("dotenv").config();
-
+const dbConfig = require("./config/dbConfig");
 const port = process.env.PORT || 10000;
 
 app.listen(port, () => console.log("Server listening on port: " + port + "!"));
